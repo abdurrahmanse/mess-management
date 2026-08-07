3583183b28f8834e9f4ced70ea3422e09073773a
Abdur Rahman <abdurrahmansoftw@gmail.com>
2026-08-07 12:40:40 +0600

    refactor: remove development testing scripts and add postinstall prisma generation task

--- scratch-hash.ts
+++
@@ -1,6 +0,0 @@
-import { hashPassword } from "better-auth/crypto";
-async function run() {
-  const hash = await hashPassword("AR.1$lpd");
-  console.log("HASH:", hash);
-}
-run();
--- test-auth.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  const user = await prisma.user.findUnique({ where: { email: 'manager1@example.com' }, include: { accounts: true } });
-  console.log(user);
-}
-main().finally(() => prisma.$disconnect());
